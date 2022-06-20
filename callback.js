7d496030cfcbfb94e5968ca6b1c0df49ba473a35
Rajesh Nepal <neprazesh@gmail.com>
2022-06-20 07:54:10 +0545

    err & success in callback

--- callback.js
+++ callback.js
@@ -20,18 +20,29 @@
 
 // callback is a result handling mechanism for asynchronous time consuming task
 
-function askMoney(amt,cb){  //cb is a function
+function askMoney(amt, cb) {  //cb is a function
+    // console.log('what comes in',cb);
     console.log('I asked money with mom');
     console.log('Mom told me to wait until she gets her salary');
-    setTimeout(function(){
+    setTimeout(function () {
         console.log('Mom got her salary');
-        cb(amt + 1000) ;
+        var momIsHappy = true
+        if (momIsHappy) {
+            cb(null, amt + 1000);
+        } else {
+            cb(err);
+        }
         // time laagne case ma return doesnot work
-    },3000)
+    }, 3000)
 }
 
-askMoney(1000,function(){ //this function called when cb is called in askmoney
-    console.log('Money received from mom');
-    console.log('Go to buy mobile');
+askMoney(1000, function (err, success) { //this function called when cb is called in askmoney
+    if (err) {
+        console.log('there is err in askMoney ', err);
+    } else {
+        console.log('Money received from mom');
+        console.log('Go to buy mobile');
+    }
+
 })
 console.log('Studying until mom gives me money'); //this is non-blocking i.e result of waiting function doesnot bloack this task
